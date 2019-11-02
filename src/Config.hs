39da7431f74cf4d9cc2005a60c353e7317f2b6a3
Daniel Martinez <daniel.martinez@ansarada.com>
2019-11-02 11:00:36 +1100

    Return Either instead of maybe
    Reformat the file
    Use Monoid 'last' instead of semigroup
    Use string utils function

--- src/Config.hs
+++ src/Config.hs
@@ -1,13 +1,18 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Config
-  ( GenConfig(projectDir, language)
+  ( GenConfig(projectDir, language, templatesDir)
   , Language(..)
   , getConfig
   ) where
 
-import           Data.Functor     ((<&>))
-import           Data.Maybe       (Maybe (Just))
-import           Data.Semigroup   (Last (Last), getLast)
-import           System.Directory (findFile, getCurrentDirectory)
+import qualified Data.ByteString.Char8  as C
+import           Data.Functor           ((<&>))
+import           Data.JsonStream.Parser (parseByteString, value, (.:))
+import           Data.Monoid            (Last (Last), getLast)
+import qualified Data.Text              as T
+import           System.Directory       (findFile, getCurrentDirectory)
+import           Utils                  (upperCase)
 
 data Language
   = JavaScript
@@ -16,8 +21,9 @@ data Language
 
 data GenConfigOption =
   GenConfigOption
-    { projectDirOption :: Last (Maybe String)
-    , languageOption   :: Last (Maybe Language)
+    { projectDirOption   :: Last String
+    , languageOption     :: Last Language
+    , templatesDirOption :: Last String
     }
 
 instance Semigroup GenConfigOption where
@@ -25,12 +31,14 @@ instance Semigroup GenConfigOption where
     GenConfigOption
       { projectDirOption = projectDirOption a <> projectDirOption b
       , languageOption = languageOption a <> languageOption b
+      , templatesDirOption = templatesDirOption a <> templatesDirOption b
       }
 
 data GenConfig =
   GenConfig
-    { projectDir :: String
-    , language   :: Language
+    { projectDir   :: String
+    , language     :: Language
+    , templatesDir :: String
     }
 
 dotfile :: String
@@ -38,16 +46,48 @@ dotfile = ".progenrc"
 
 makeDefaultConfig :: GenConfigOption
 makeDefaultConfig =
-  GenConfigOption (Last $ Just "/Users/daniel.martinez/Documents/js/dummy-project/") (Last $ Just Flow)
+  GenConfigOption
+    (Last $ Just "/Users/daniel.martinez/Documents/js/dummy-project/")
+    (Last $ Just JavaScript)
+    (Last $ Just "/Users/daniel.martinez/Documents/js/dummy-project/.progenrc/templates/")
+
+safeHead :: [String] -> Maybe String
+safeHead (h:_) = Just h
+safeHead _     = Nothing
+
+safeGetProjectDirValue :: String -> String -> Maybe String
+safeGetProjectDirValue key content =
+  safeHead (parseByteString (T.pack key .: value) (C.pack content) :: [String])
+
+toLang :: String -> Maybe Language
+toLang str =
+  case upperCase str of
+    "JAVASCRIPT" -> Just JavaScript
+    "FLOW"       -> Just Flow
+    "TYPESCRIPT" -> Just TypeScript
+    _            -> Nothing
 
 findConfig :: IO GenConfigOption
 findConfig =
   getCurrentDirectory >>=
-  (\pwd -> findFile [pwd] dotfile) <&>
-  (\f -> GenConfigOption (Last $ Just "/Users/daniel.martinez/Documents/js/dummy-project/") (Last $ Just JavaScript))
+  (\pwd -> findFile [pwd] dotfile >>= traverse readFile) <&>
+  (\case
+     Just content ->
+       GenConfigOption
+         (Last $ safeGetProjectDirValue "projectDir" content)
+         (Last $ toLang =<< safeGetProjectDirValue "language" content)
+         (Last $ safeGetProjectDirValue "templatesDir" content)
+     Nothing -> GenConfigOption (Last Nothing) (Last Nothing) (Last Nothing))
 
-getConfig :: IO (Maybe GenConfig)
-getConfig =
-  (\(GenConfigOption projectDir' language') -> GenConfig <$> getLast projectDir' <*> getLast language') .
-  (<> makeDefaultConfig) <$>
-  findConfig
+getConfig :: IO (Either String GenConfig)
+getConfig = do
+  conf <- findConfig
+  let combined = makeDefaultConfig <> conf
+  let result =
+        GenConfig <$> getLast (projectDirOption combined) <*>
+        getLast (languageOption combined) <*>
+        getLast (templatesDirOption combined)
+  pure $
+    case result of
+      Just config -> Right config
+      Nothing     -> Left "ERROR: There is valid no configuration."
