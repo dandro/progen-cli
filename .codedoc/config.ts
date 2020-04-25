9d47c7e53d93a957253a1186802ee6ea9a2ed850
Daniel Martinez <daniel.martinez@ansarada.com>
2020-04-26 00:46:25 +1000

    Fix documentation namespace

--- .codedoc/config.ts
+++ .codedoc/config.ts
@@ -5,19 +5,20 @@ import { theme } from './theme';
 
 
 export const config = /*#__PURE__*/configuration({
-  theme,                                  // --> add the theme. modify `./theme.ts` for chaning the theme.
-  
+  theme,                                  // --> add the theme. modify `./theme.ts` for changing the theme.
   page: {
     title: {
       base: 'Progen Cli'                  // --> the base title of your doc pages
     }
   },
-  
   misc: {
     github: {
       user: 'dandro',
       repo: 'progen-cli',
       large: true,
     }
+  },
+  dest: {
+    namespace: '/progen-cli'
   }
 });
